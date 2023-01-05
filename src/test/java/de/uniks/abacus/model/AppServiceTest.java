73d8c50d8c3d9d2426cdd92ec549bb6d33109292
MusLead <aghaaslam246@gmail.com>
2023-01-05 11:28:12 +0100

    feat: creating AppServiceTest

---
+++ src/test/java/de/uniks/abacus/model/AppServiceTest.java
@@ -0,0 +1,43 @@
+package de.uniks.abacus.model;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppServiceTest {
+    final AppService AS = new AppService();
+    @Test
+    public void computerCalculateTest() {
+        /*
+        * Calculate gives a value of the computer calculation
+        * */
+        Player player = new Player();
+        int firstVal = 2, secondVal = 3;
+
+        char operation = '*';
+        assertEquals(firstVal * secondVal, AS.calculate(firstVal,operation,secondVal));
+
+    }
+
+    @Test
+    public void playerCalculationTest() {
+        Player player = new Player();
+        int firstVal = 2, secondVal = 3;
+        int rightVal = AS.calculate(firstVal,'+',secondVal);
+
+        player.setName("Agha").withHistories(new History().withResults(
+                new Result().setFirstVal(firstVal)
+                        .setSecondVal(secondVal)
+                        .setOperation('+')
+                        .setRightVal(rightVal)
+        ));
+
+        Result resultPlayer = player.getHistories().get(0).getResults().get(0);
+        int playerInput = 7;
+        boolean howResult = AS.checkResult(resultPlayer.setResultVal(playerInput));
+
+        assertEquals(rightVal == playerInput, howResult);
+
+    }
+}
