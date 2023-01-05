9bca7430dc87bd4067e318224254eb71f104c317
MusLead <aghaaslam246@gmail.com>
2023-01-05 18:09:13 +0100

    refactor: add and revise AooService

--- src/main/java/de/uniks/abacus/model/AppService.java
+++ src/main/java/de/uniks/abacus/model/AppService.java
@@ -1,9 +1,17 @@
 package de.uniks.abacus.model;
 
+import de.uniks.abacus.Constant;
+
+import java.util.Random;
+
+import static de.uniks.abacus.Constant.*;
+
 public class AppService {
-    public int calculate(int firstVal, char operation, int secondVal){
+
+    final private Random random = new Random();
+    private int calculate(int firstVal, char operation, int secondVal){
         int resultVal = 0;
-        switch (operation){
+        switch (operation) {
             case '+' -> resultVal = firstVal + secondVal;
             case '-' -> resultVal = firstVal - secondVal;
             case '*' -> resultVal = firstVal * secondVal;
@@ -19,7 +27,110 @@ public class AppService {
         return resultVal;
     }
 
-    public boolean checkResult(Result result) {
-        return result.getRightVal() == result.getResultVal();
+    /**
+     * Check the result whether the user answer correct or wrong
+     * either wrong or correct.
+     * the player and the history should increase their
+     * sum and total of (wrong or correct).
+     *
+     * This method should only be used if the
+     * right result has been calculated and the result has
+     * a player and a history
+     *
+     * @param result object that fulfill the condition
+     * @return the right result as true
+     */
+    private boolean checkResult(Result result) {
+        if(result.getPlayer() == null || result.getHistory() == null || result.getRightVal() == 0){
+            throw new IllegalArgumentException("The program is incomplete!");
+        }
+        boolean isCorrect = result.getRightVal() == result.getResultVal();
+        Player currentPlayer = result.getPlayer();
+        History currentHistory = result.getHistory();
+        if(isCorrect){
+            currentPlayer.setRightSum(currentPlayer.getRightSum() + 1);
+            currentHistory.setRightResultTotal(currentHistory.getRightResultTotal() + 1);
+            result.setResultStatus(CORRECT);
+        } else {
+            currentPlayer.setWrongSum(currentPlayer.getWrongSum() + 1);
+            currentHistory.setWrongResultTotal(currentHistory.getWrongResultTotal() + 1);
+            result.setResultStatus(WRONG);
+        }
+        return isCorrect;
+    }
+
+    /**
+     * This method find a new task if the current Task has the result of not natural numbers
+     * @param player to get the value of current player
+     * @param origin the lowest value for the random
+     * @param upperBound the highest value for the random
+     */
+    protected void checkDivision(Player player, int origin, int upperBound) {
+        int historyIndex = player.getHistories().size() - 1;
+        int resultIndex = player.getHistories().get(historyIndex).getResults().size() - 1;
+        Result currentResult = player.getHistories().get(historyIndex).getResults().get(resultIndex);
+        float firstVal =  currentResult.getFirstVal();
+        float secondVal = currentResult.getSecondVal();
+        int intVal = (int) (firstVal/secondVal);
+        /*
+        * example:
+        * fistVal = 3 secondVal = 2;
+        * intVal = 1
+        * firstval/secondVal = 1.5
+        * because 1 != 1.5 find another number until they have the same value!
+        * */
+        while ((firstVal/secondVal) != intVal) {
+            firstVal = random.nextInt(origin,upperBound);
+            secondVal = random.nextInt(origin, (int) firstVal);
+            intVal = (int) (firstVal/secondVal);
+        }
+
+        // after quit from the loop, set all the new value to the player!
+        currentResult.setFirstVal((int) firstVal)
+                .setSecondVal((int) secondVal);
+    }
+
+    /**
+     * This function only be used after the user gives an Answer
+     * We want to know everytime the new results is being created, the results should be able
+     * to access either from player or from the history.
+     *
+     * The origin and bound are for the checkDivision, to find another alternative number
+     * if the right result is not within the set of Natural Number
+     * @param player current player
+     * @param firstVal first value
+     * @param operation operation
+     * @param secondVal second value
+     * @param origin for the random in checkDivision
+     * @param bound for the random in checkDivision
+     * @return current result
+     */
+    public Result creatNewResult(Player player, int firstVal, char operation, int secondVal, int userInput
+            , int origin, int bound) {
+        if(operation == '/') // check for other possibility! look at the method description!
+            checkDivision(player, origin, bound);
+
+        Result result = new Result()
+                .setPlayer(player)
+                .setFirstVal(firstVal)
+                .setSecondVal(secondVal)
+                .setOperation(operation)
+                .setRightVal(calculate(firstVal,operation,secondVal))
+                .setResultVal(userInput);
+
+        int currentHistorySize = player.getHistories().size();
+        if(currentHistorySize != 0) {
+            //take the last history that is being created and put the result in
+            History currentHistory = player.getHistories().get(currentHistorySize - 1);
+            currentHistory.withResults(result);
+        } else {
+            History currentHistory = new History().withResults(result);
+            player.withHistories(currentHistory);
+        }
+
+        // check the result that has a Player and also a History
+        checkResult(result);
+
+        return result;
     }
 }
--- src/test/java/de/uniks/abacus/model/AppServiceTest.java
+++ src/test/java/de/uniks/abacus/model/AppServiceTest.java
@@ -1,42 +1,74 @@
 package de.uniks.abacus.model;
 
 
+import de.uniks.abacus.Constant;
 import org.junit.jupiter.api.Test;
 
+import static de.uniks.abacus.Constant.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppServiceTest {
+
+    /*
+     * PROGRAMMING'S RULE:
+     * The Player should be created each time the user want to create a new player.
+     * The Task should be created within createNewResult() Method!
+     * The userInput of the calculation's result should be ONLY accessed by createNewResult()
+     * The Player should not take the userInput's value directly!
+     */
+
     final AppService AS = new AppService();
     @Test
     public void computerCalculateTest() {
         /*
-        * Calculate gives a value of the computer calculation
+        * Calculate() gives a value of the computer calculation
         * */
-        int firstVal = 2, secondVal = 3;
-
+        int firstVal = 2, secondVal = 3, playerInput = 7;
         char operation = '*';
-        assertEquals(firstVal * secondVal, AS.calculate(firstVal,operation,secondVal));
+
+        Player player = new Player();
+        Result result = AS.creatNewResult(player, firstVal, operation, secondVal, playerInput,0, 1000);
+
+        assertEquals(firstVal * secondVal, result.getRightVal());
 
     }
 
     @Test
     public void playerCalculationTest() {
-        Player player = new Player();
+        Player player = new Player().setName("Agha");
         int firstVal = 2, secondVal = 3;
-        int rightVal = AS.calculate(firstVal,'+',secondVal);
+        int playerInput = 7;
 
-        player.setName("Agha").withHistories(new History().withResults(
+        Result rightVal = AS.creatNewResult(player,firstVal,'+',secondVal,playerInput,0,10000);
+
+        assertEquals(rightVal.getRightVal() == playerInput, rightVal.getResultStatus().contains(CORRECT));
+    }
+
+    @Test
+    public void checkDivisionTest(){
+        int firstVal = 3, secondVal = 2;
+        Player player = new Player().withHistories(new History().withResults(
                 new Result().setFirstVal(firstVal)
                         .setSecondVal(secondVal)
-                        .setOperation('+')
-                        .setRightVal(rightVal)
+                        .setOperation('/')
         ));
 
-        Result resultPlayer = player.getHistories().get(0).getResults().get(0);
-        int playerInput = 7;
-        boolean howResult = AS.checkResult(resultPlayer.setResultVal(playerInput));
+        AS.checkDivision(player, 0, 1000);
 
-        assertEquals(rightVal == playerInput, howResult);
+        int historyIndex = player.getHistories().size() - 1;
+        int resultIndex = player.getHistories().get(historyIndex).getResults().size() - 1;
+        Result currentResult = player.getHistories().get(historyIndex).getResults().get(resultIndex);
 
+        int firstValNow = currentResult.getFirstVal();
+        int secondValNow = currentResult.getSecondVal();
+        assertNotEquals(firstVal,firstValNow);
+        assertNotEquals(secondVal, secondValNow);
+
+        /* why (float) ?
+         * https://stackoverflow.com/questions/65678297/solve-integer-division-in-floating-point-context
+         */
+        float resultTest = (float) (firstVal/secondVal)*10;
+        //it should return true, if the (result*10)%10 should be 0
+        assertEquals(0, (resultTest)%10);
     }
 }
