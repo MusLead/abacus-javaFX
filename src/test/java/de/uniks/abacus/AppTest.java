df567a489da1a7d65864761d895598fb7cdce858
MusLead <aghaaslam246@gmail.com>
2023-01-09 03:14:59 +0100

    perf: ERROR in AppTest

--- src/test/java/de/uniks/abacus/AppTest.java
+++ src/test/java/de/uniks/abacus/AppTest.java
@@ -1,10 +1,12 @@
 package de.uniks.abacus;
 
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+
 
 class AppTest extends ApplicationTest {
 
@@ -12,7 +14,7 @@ class AppTest extends ApplicationTest {
     private App app;
 
     @Override
-    public void start( Stage stage ) throws Exception {
+    public void start( Stage stage ) {
         this.stage = stage;
         app = new App();
         app.start(stage); //to App.java
@@ -20,6 +22,30 @@ class AppTest extends ApplicationTest {
 
     @Test
     public void changeView(){
+        //• Initialen Fenstertitel prüfen
+        assertEquals(Constant.HOMEPAGE_TITLE,stage.getTitle());
 
+        //• Spielernamen „Alice“ in das dafür vorgesehenen Eingabefeld eingeben.
+        clickOn("#nameInput");
+        final String name = "Alice";
+        write(name);
+        TextField userName = lookup("#nameInput").queryAs(TextField.class);
+        assertEquals(name, userName.getText());
+
+        //• Start-Button klicken, um ein Encounter zu starten
+        clickOn("#startButton");
+
+        //• Neuen Fenstertitel prüfen
+        assertEquals(Constant.OPTION_TITLE,stage.getTitle());
+
+        //• Leave-Button klicken
+        clickOn("#mainMenuButton");
+
+        //• Fenstertitel erneut prüfen
+        assertEquals(Constant.HOMEPAGE_TITLE,stage.getTitle());
+
+        //• Prüfen, dass das Eingabefeld für den Spielernamen leer ist
+        userName = lookup("#nameInput").queryAs(TextField.class);
+        assertEquals("", userName.getText());
     }
 }
