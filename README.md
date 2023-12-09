b57f00c5cec1386257cc03dbad507c1329c5296f
MusLead <aghaaslam246@gmail.com>
2023-12-09 19:27:51 +0100

    refactor: update FXML files
    to avoid warning from execution

--- README.md
+++
@@ -1,5 +0,0 @@
-# Abacus-javaFX
-
-This is a simple arithmetic app
-
-If you want to see some Conclusion on how I make this program, please check [PROGRAMMING.md](PROGRAMMING.md)  
--- src/main/java/de/uniks/abacus/Main.java
+++
@@ -1,9 +0,0 @@
-package de.uniks.abacus;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main( String[] args ) {
-        Application.launch(App.class, args);
-    }
-}
